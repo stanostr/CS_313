e61f40731f03217a6d2dc252a2fb4824718fb215
Stan <stanislav.ostrovskii10@my.stjohns.edu>
2019-01-22 17:56:59 -0500

    Add files via upload
    Added example to demonstrate stack trace.

---
+++ lec1_exceptions/ExceptionStackTrace.java
@@ -0,0 +1,32 @@
+package lec1_exceptions;
+
+public class ExceptionStackTrace {
+    public static void main(String[] args)
+    {
+        methodOne(0);
+        System.out.println("Carrying on...");
+    }
+
+    private static void methodOne(int num)
+    {
+        methodTwo(num);
+    }
+
+    private static void methodTwo(int num)
+    {
+        methodThree(num);
+
+        /**
+        try {
+            methodThree(num);
+        } catch (ArithmeticException a) {
+            System.out.println("An ArithmeticException occurred but we caught it!");
+        } */
+    }
+
+    private static void methodThree(int num)
+    {
+        System.out.println(1/num);
+    }
+
+}
