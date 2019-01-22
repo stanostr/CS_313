f70a79cd15c0d7c95be0c45fa43cd48002e19913
Stan <stanislav.ostrovskii10@my.stjohns.edu>
2019-01-22 18:41:20 -0500

    Add files via upload

---
+++ lec1_exceptions/ExceptionDemoUnchecked.java
@@ -0,0 +1,49 @@
+package lec1_exceptions;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExceptionDemoUnchecked {
+    public static void main(String[] args)
+    {
+        readInArrayFromFile();
+    }
+
+    //method to read in some numbers from a file named "file.txt" and print them to console
+    private static void readInArrayFromFile() {
+        List<Integer> list = new ArrayList<Integer>();
+        File file = new File("file.txt");
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String text = null;
+
+            while ((text = reader.readLine()) != null) {
+                list.add(Integer.parseInt(text));
+            }
+        } catch (FileNotFoundException e)
+        {
+            //in the real world, you would want these blocks to take appropriate actions,
+            //to continue execution of program
+            //and not just print errors
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("IOException occurred");
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                //because unchecked exceptions MUST be caught, some programmers just add
+                //the mandatory catch block and leave it blank but this is bad practice
+            }
+        }
+
+        //print out the list
+        System.out.println(list);
+    }
+
+}
--- lec1_exceptions/ExceptionStackTrace.java
+++ lec1_exceptions/ExceptionStackTrace.java
@@ -1,3 +1,37 @@
+<<<<<<< HEAD
+package lec1_exceptions;
+
+public class ExceptionStackTrace {
+    public static void main(String[] args)
+    {
+        methodOne(0);
+        System.out.println("Carrying on...");
+    }
+
+    private static void methodOne(int num)
+    {
+        methodTwo(num);
+    }
+
+    private static void methodTwo(int num)
+    {
+        methodThree(num);
+
+        /**
+        try {
+            methodThree(num);
+        } catch (ArithmeticException a) {
+            System.out.println("An ArithmeticException occurred but we caught it!");
+        } */
+    }
+
+    private static void methodThree(int num)
+    {
+        System.out.println(1/num);
+    }
+
+}
+=======
 package lec1_exceptions;
 
 public class ExceptionStackTrace {
@@ -23,3 +57,4 @@ public class ExceptionStackTrace {
     }
 
 }
+>>>>>>> origin/master
