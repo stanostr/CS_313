fef75b069c55a0bef2c7eb788df65abc3b81983c
Stan <stanislav.ostrovskii10@my.stjohns.edu>
2019-02-06 21:46:10 -0500

    Fixed misleading comments

--- linkedlist/SinglyLinkedList.java
+++ linkedlist/SinglyLinkedList.java
@@ -13,7 +13,7 @@ public class SinglyLinkedList<E> {
 			this.element = element;
 			this.next = next;
 		}
-		public E getElement() { return element; } //setter for the data
+		public E getElement() { return element; } //getter for the data
 		public Node<E> getNext() { return next; } //get next Node
 		public void setNext(Node<E> next) { this.next = next; } //change next Node
 	}
