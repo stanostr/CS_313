3ae2e253a001a6407a3d82182fc6b1ce96171548
Stan <stanislav.ostrovskii10@my.stjohns.edu>
2019-02-26 17:59:13 -0500

    Update ArrayListTest2.java

--- list/ArrayListTest2.java
+++ list/ArrayListTest2.java
@@ -10,9 +10,9 @@ public class ArrayListTest2 {
         arrayList.add(4);
         arrayList.add(8);
         arrayList.add(9);
-        System.out.println(arrayList.get(1));
-        System.out.println(arrayList.set(0, 7)); //line 1
-        System.out.println(arrayList.remove(1)); //line 2
-        System.out.println(arrayList.get(1)); //line 3
+        System.out.println(arrayList.get(1)); //line 1
+        System.out.println(arrayList.set(0, 7)); //line 2
+        System.out.println(arrayList.remove(1)); //line 3
+        System.out.println(arrayList.get(1)); //line 4
     }
 }
