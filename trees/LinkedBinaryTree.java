ee8ba23b0d58744e7e04d0f2e7651714191d8e1e
stanostr <stan_ostr@yahoo.com>
2019-03-06 01:33:06 -0500

    Fixed a comment. Began implementing concrete BinaryTree class.

--- trees/LinkedBinaryTree.java
+++ trees/LinkedBinaryTree.java
@@ -2,9 +2,7 @@ package trees;
 
 import java.util.Iterator;
 
-//please come back later!!!!
 public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
-
     //<b>In progress, please come back later!!</b>
 
     //nested Node class
@@ -38,30 +36,14 @@ public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
     {
         return new Node<E>(e, parent, left, right);
     }
-    //please come back later!!!!
-    //In progress!!!
 
     protected Node<E> root = null;
     private int size = 0;
 
-    @Override
-    public Position<E> left(Position<E> p) throws IllegalArgumentException {
-        return null;
-    }
-
-    @Override
-    public Position<E> right(Position<E> p) throws IllegalArgumentException {
-        return null;
-    }
-
+    //start of accessor methods not implemented in AbstractBinaryTree
     @Override
     public Position<E> root() {
         return root;
-    }
-
-    @Override
-    public Position<E> parent(Position<E> p) throws IllegalArgumentException {
-        return null;
     }
 
     @Override
@@ -70,12 +52,83 @@ public class LinkedBinaryTree<E> extends AbstractBinaryTree<E> {
     }
 
     @Override
+    public Position<E> left(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getLeft();
+    }
+
+    @Override
+    public Position<E> right(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getLeft();
+    }
+
+    @Override
+    public Position<E> parent(Position<E> p) throws IllegalArgumentException {
+        Node<E> node = validate(p);
+        return node.getLeft();
+    }
+    //end of accessor methods not implemented in AbstractBinaryTree
+
+    //start of update methods for this class
+
+    /** adds a root to an empty tree */
+    public Position<E> addRoot(E e) throws IllegalStateException
+    {
+        //cannot add a root to a tree that already has one
+        if(!isEmpty()) throw new IllegalStateException("Tree is not empty");
+        root = createNode(e, null, null, null);
+        size = 1;
+        return root;
+    }
+
+    public Position<E> addLeft(E e) throws IllegalArgumentException
+    {
+        return null;
+    }
+
+    public Position<E> addRight(E e) throws IllegalArgumentException
+    {
+        return null;
+    }
+
+    /** Replaces element at position p, and returns replaced element */
+    public E set(Position<E> p, E e) throws IllegalArgumentException
+    {
+        Node<E> node = validate(p);
+        E temp = node.getElement();
+        node.setElement(e);
+        return temp;
+    }
+
+
+    //end of updates methods for this class
+
+    @Override
     public Iterator<E> iterator() {
+        //We are not ready to implement this yet!!!
+        //we need to learn about tree traversal first
         return null;
     }
 
     @Override
     public Iterable<Position<E>> positions() {
+        //We are not ready to implement this yet!!!
+        //we need to learn about tree traversal first
         return null;
+    }
+
+    /**
+     * Validates the position and returns it as a node
+     * @throws IllegalArgumentException if given invalid node
+     */
+    private Node<E> validate(Position<E> p) throws IllegalArgumentException {
+        //note that if this test fails, it is still a Position, just note a Node
+        if(!(p instanceof Node))
+            throw new IllegalArgumentException("Not a valid position type");
+        Node<E> node = (Node<E>) p;
+        if(node.getParent() == node) //when we remove Nodes we set their parent to themselves.
+            throw new IllegalArgumentException("p is no longer in the tree");
+        return node;
     }
 }
