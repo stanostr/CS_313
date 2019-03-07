65db418707366706ba2f89d85225a1725eb31e35
stanostr <stan_ostr@yahoo.com>
2019-03-07 01:28:58 -0500

    Added experimental analysis test for R-7.9 homework exercise.

---
+++ list/exercise/ExperimentalAnalysis.java
@@ -0,0 +1,26 @@
+package list.exercise;
+import list.List;
+
+public class ExperimentalAnalysis {
+    public static void main(String[] args)
+    {
+        List<Integer> newList = new list.ArrayList<>(1);
+        List<Integer> oldList = new ArrayList<>(1);
+        long startNew = System.currentTimeMillis();
+        for(int i=0; i<21000000; i++)
+        {
+            newList.add(0);
+        }
+        long elapsed = System.currentTimeMillis() - startNew;
+        System.out.println("Using new list: " + elapsed);
+
+        long startOld = System.currentTimeMillis();
+        for(int i=0; i<21000000; i++)
+        {
+            oldList.add(0);
+        }
+        elapsed = System.currentTimeMillis() - startOld;
+        System.out.println("Using old list: " + elapsed);
+
+    }
+}
