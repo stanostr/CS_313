2041ae81e308999e54dda5ce1ce3c0a354b22ed3
stanostr <stan_ostr@yahoo.com>
2019-03-20 03:25:22 -0400

    Added code from the midterm exam #3 to demonstrate that it compiles with no problem. Very few students were able to answer this question correctly.

---
+++ misc/TheQuestion.java
@@ -0,0 +1,29 @@
+package misc;
+
+public class TheQuestion {
+    /**
+     * This is the code from the midterm exam #3.
+     * It compiles with no problem, as you can test for yourself.
+     */
+
+    private interface myInterface
+    {
+        void hello();
+        void bye();
+    }
+    private abstract class myAbstractClass implements myInterface
+    {
+        public void hello() { System.out.println("Hi"); }
+        public abstract void hello2();
+    }
+
+    private class myConcreteClass extends myAbstractClass
+    {
+        public void bye() {}
+        public void hello2() {
+            System.out.println("Hi again");
+        }
+    }
+
+
+}
