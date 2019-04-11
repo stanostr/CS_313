49d7bdb5a1a0f80439a8f9242dea05f267db3e00
stanostr <stan_ostr@yahoo.com>
2019-04-11 15:10:55 -0400

    Added MaxWord example as well as a file for testing it.

---
+++ map/WordCount.java
@@ -0,0 +1,42 @@
+package map;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args)
+    {
+        try {
+            System.out.println("Most common word is: " + maxWord("textfile.txt"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String maxWord(String filePath) throws IOException {
+        Scanner in = new Scanner(new FileReader(filePath)).useDelimiter("[^a-zA-Z]+");
+        Map<String, Integer> freqMap = new UnsortedTableMap<>();
+        while(in.hasNext()) //fill in map
+        {
+            String word = in.next().toLowerCase();
+            Integer count = freqMap.get(word);
+            if(count==null) count=0;
+            freqMap.put(word, ++count);
+        }
+        in.close();
+
+        //find most frequent
+        int maxCount = 0;
+        String maxWord = "(no word)";
+        for(Entry<String, Integer> entry: freqMap.entrySet())
+        {
+            if(entry.getValue() > maxCount) {
+                maxWord = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+        return maxWord;
+    }
+
+}
